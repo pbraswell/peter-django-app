4e59940f4d3eafaf72d3e3cec558766dc47fa3fb
Peter Braswell <pbraswell@heroku.com>
2014-10-16 11:48:11 -0400

    trying to get loaderio module to work

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'hellodjango.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls),
     url('^$', include('loaderio.urls')),
+
 )
